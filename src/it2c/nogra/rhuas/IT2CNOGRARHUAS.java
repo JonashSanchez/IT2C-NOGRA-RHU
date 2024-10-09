9f227103f17c0a7c3fa3a67017f7208bf44cc2a8
VEGATIP <alexded192@gmail.com>
2024-10-09 14:26:37 +0800

    updated view update delete

--- src/it2c/nogra/rhuas/IT2CNOGRARHUAS.java
+++ src/it2c/nogra/rhuas/IT2CNOGRARHUAS.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class IT2CNOGRARHUAS {
     
-      private final config conf = new config();
+      
 
     public void addPatients() {
         Scanner sc = new Scanner(System.in);
@@ -24,18 +24,53 @@ public class IT2CNOGRARHUAS {
         String patientsQuery = "SELECT * FROM patients";
         String[] patientsHeaders = {"ID", "First Name", "Last Name", "Email", "Appointment Date"};
         String[] patientsColumns = {"p_id", "p_name", "p_lname", "p_email", "p_appointmentdate"};
-
+        config conf = new config();
         conf.viewRecords(patientsQuery, patientsHeaders, patientsColumns);
     }
      
-     private void deletePatient() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Patient ID to delete:");
-        int patientId = sc.nextInt();
-        String sql = "DELETE FROM patients WHERE p_id = ?";
-        conf.deleteRecord(sql, String.valueOf(patientId));
-    }
+     private void updatePatient(){
+         Scanner sc = new Scanner(System.in);
+             System.out.println("Enter Patient ID:");
+             int id = sc.nextInt();
+          
+             
+           System.out.println("Enter the new first name:");
+           String ufname = sc.next();
+           
+             System.out.println("Enter the new last name:");
+           String ulname = sc.next();
+           
+             System.out.println("Enter new last email:");
+           String uemail = sc.next();
+           
+             System.out.println("Enter new appointment date:");
+           String udate = sc.next();
+             
+           String qry = "UPDATE patients SET p_name = ?, p_lname = ?, p_email = ?, p_appointmentdate = ? WHERE p_id =?";
+           
+           config conf = new config();
+           conf.updateRecord(qry, ufname, ulname, uemail, udate, id);
+         
+     }
+             
+             
+             
+    private void deletePatient(){
+         Scanner sc = new Scanner(System.in);
+             System.out.println("Enter Patient ID to delete:");
+             int id = sc.nextInt();
+             String sqlDelete = "DELETE FROM patients WHERE p_id = ?";
+           config conf = new config();
+           conf.deletePatient(sqlDelete, id);
+           
+    }       
+    
+    
      
+     
+    
+     
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         IT2CNOGRARHUAS demo = new IT2CNOGRARHUAS();
@@ -59,10 +94,18 @@ public class IT2CNOGRARHUAS {
                 case 2:
                     demo.viewPatients();
                     break;
+                
+                case 3:
+                    demo.viewPatients();
+                    demo.updatePatient();
+                    break;
                
                     
                 case 4:
+                    demo.viewPatients();
+                    
                     demo.deletePatient();
+                    demo.viewPatients();
                     break;
                 case 5:
                     System.exit(0);
